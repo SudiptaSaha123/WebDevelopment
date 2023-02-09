65ac6fcc6d4b740534fe078ba9ef3f31eb802ea4
“SudiptaSaha123” <sudiptasaha883@gmail.com>
2023-02-10 00:02:49 +0530

    index.js file added

--- Javascript/1/index.js
+++ Javascript/1/index.js
@@ -1,9 +1,77 @@
 console.log("External Javascript");
-let a = 999;
-console.log(a);
+// let a = 999;
+// console.log(a);
 
-let value = 5;
-console.log(value);
+// let value = 5;
+// console.log(value);
 
-value = true;
-console.log(value);
+// value = true;
+// console.log(value);
+
+// let a = true;
+// console.log(a);
+
+// const num = 12;
+// num = 13;
+
+// let lastName = 3;
+// console.log(lastName);
+
+// lastName = true;
+// console.log(lastName);
+
+// console.log(5 !== 3);
+
+// let age = 27;
+// let status = (age >= 18) ? 'Yes Vote' : 'No Vote';
+
+// console.log(status);
+
+// let marks = 12;
+
+// if(marks >= 90) {
+//     console.log('A');
+// }
+// else if(marks >= 80) {
+//     console.log('B');
+// }
+// else if(marks >=70) {
+//     console.log('C');
+// }
+// else if(marks >= 60) {
+//     console.log('D');
+// }
+// else {
+//     console.log('E');
+//}
+
+// let num = 1;
+
+// switch(num) {
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default: console.log('D');
+// }
+
+
+// for(let i = 1; i<=5; i=i+1) {
+//     console.log(i);
+// }
+
+// let x = 1;
+
+// while(x <= 5) {
+//     console.log(x);
+//     x++;
+// }
+
+// let y = 1;
+
+// do {
+//     console.log(y);
+//     y++;
+// } while(y < 10);
